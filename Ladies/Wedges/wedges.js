7cd95e17040896f949aabded6cfa98b5ca76dcd0
rendaniluk <lukhwa11582924@gmail.com>
2017-04-09 20:53:24 +0200

    adding functionality to wedge,sandals and flat link

---
+++ Ladies/Wedges/wedges.js
@@ -0,0 +1,189 @@
+var darkB = document.querySelector('.darkbrown');
+var white = document.querySelector('.white');
+var gray = document.querySelector('.gray');
+var Blue = document.querySelector('.blue');
+var Green = document.querySelector('.green');
+var black = document.querySelector('.black');
+var template = document.querySelector('.template')
+var shoeDetails_flats = document.querySelector('.shoeDetails_flats')
+
+
+var Ladies = [{
+    color: 'Dark Brown',
+    size: 4,
+    price: 500,
+    in_stock: 5
+  }, {
+    color: 'Dark Brown',
+    size: 5,
+    price: 500,
+    in_stock: 2
+  }, {
+    color: 'Dark Brown',
+    size: 6,
+    price: 500,
+    in_stock: 10
+  }, {
+    color: 'Dark Brown',
+    size: 7,
+    price: 500,
+    in_stock: 9
+  }, {
+    color: 'Dark Brown',
+    size: 8,
+    price: 500,
+    in_stock: 3
+  }, {
+    color: 'White',
+    size: 4,
+    price: 258,
+    in_stock: 5
+  }, {
+    color: 'White',
+    size: 5,
+    price: 258,
+    in_stock: 2
+  }, {
+    color: 'White',
+    size: 6,
+    price: 258,
+    in_stock: 10
+  }, {
+    color: 'White',
+    size: 7,
+    price: 258,
+    in_stock: 9
+  }, {
+    color: 'White',
+    size: 8,
+    price: 258,
+    in_stock: 3
+  }, {
+    color: 'Green',
+    size: 4,
+    price: 458,
+    in_stock: 5
+  }, {
+    color: 'Green',
+    size: 5,
+    price: 458,
+    in_stock: 2
+  }, {
+    color: 'Green',
+    size: 6,
+    price: 458,
+    in_stock: 10
+  }, {
+    color: 'Green',
+    size: 7,
+    price: 458,
+    in_stock: 9
+  }, {
+    color: 'Green',
+    size: 8,
+    price: 458,
+    in_stock: 3
+  }, {
+    color: 'Gray',
+    size: 4,
+    price: 358,
+    in_stock: 5
+  }, {
+    color: 'Gray',
+    size: 5,
+    price: 358,
+    in_stock: 2
+  }, {
+    color: 'Gray',
+    size: 6,
+    price: 358,
+    in_stock: 10
+  }, {
+    color: 'Gray',
+    size: 7,
+    price: 358,
+    in_stock: 9
+  }, {
+    color: 'Gray',
+    size: 8,
+    price: 358,
+    in_stock: 3
+  }, {
+    color: 'Blue',
+    size: 4,
+    price: 150,
+    in_stock: 5
+  }, {
+    color: 'Blue',
+    size: 5,
+    price: 150,
+    in_stock: 2
+  }, {
+    color: 'Blue',
+    size: 6,
+    price: 150,
+    in_stock: 10
+  }, {
+    color: 'Blue',
+    size: 7,
+    price: 150,
+    in_stock: 9
+  }, {
+    color: 'Blue',
+    size: 8,
+    price: 150,
+    in_stock: 3
+  }, {
+    color: 'Black',
+    size: 4,
+    price: 500,
+    in_stock: 5
+  }, {
+    color: 'Black',
+    size: 5,
+    price: 500,
+    in_stock: 2
+  }, {
+    color: 'Black',
+    size: 6,
+    price: 500,
+    in_stock: 10
+  }, {
+    color: 'Black',
+    size: 7,
+    price: 500,
+    in_stock: 9
+  }, {
+    color: 'Black',
+    size: 8,
+    price: 500,
+    in_stock: 3
+  }
+
+];
+var myTemplate = Handlebars.compile(template.innerHTML);
+var imgValue = function(evt) {
+  var mmm = evt.target.value;
+  var shoeList = [];
+  for (var i = 0; i < Ladies.length; i++) {
+    var imgSelected = Ladies[i];
+    if (Ladies[i].color.startsWith(mmm)) {
+      shoeList.push(imgSelected);
+      var shoeInfo = myTemplate({
+        shoeDetails_flats: shoeList
+      });
+      shoeDetails_flats.innerHTML = shoeInfo;
+    } // else if (Ladies[i].color.startsWith(mmm) == null) {
+    //   var massage = "No Stock Avalailable"
+    //   shoeDetails.innerHTML = massage;
+    // }
+  }
+}
+white.addEventListener('click', imgValue);
+darkB.addEventListener('click', imgValue);
+gray.addEventListener('click', imgValue);
+Blue.addEventListener('click', imgValue);
+Green.addEventListener('click', imgValue);
+black.addEventListener('click', imgValue);
+
+
--- kids.js
+++ kids.js
@@ -4,121 +4,138 @@ var size = document.querySelector('.sizes');
 var searchBtn = document.querySelector('.search');
 var template = document.querySelector('.template')
 var shoeDetails = document.querySelector('.shoeDetails')
-//*****************DOM END***************************
+  //*****************DOM END***************************
 var kids = [{
-        color: 'Blue',
-        size: 1,
-        price: 350,
-        in_stock: 5
-    }, {
-        color: 'Blue',
-        size: 2,
-        price: 350,
-        in_stock: 2
-    }, {
-        color: 'Blue',
-        size: 3,
-        price: 350,
-        in_stock: 10
-    }, {
-        color: 'Blue',
-        size: 4,
-        price: 350,
-        in_stock: 9
-    }, {
-        color: 'Blue',
-        size: 5,
-        price: 350,
-        in_stock: 3
-    }, {
-        color: 'White',
-        size: 1,
-        price: 400,
-        in_stock: 5
-    }, {
-        color: 'White',
-        size: 2,
-        price: 400,
-        in_stock: 2
-    }, {
-        color: 'White',
-        size: 3,
-        price: 400,
-        in_stock: 10
-    }, {
-        color: 'White',
-        size: 4,
-        price: 400,
-        in_stock: 9
-    }, {
-        color: 'White',
-        size: 5,
-        price: 400,
-        in_stock: 3
-    }, {
-        color: 'Black',
-        size: 1,
-        price: 600,
-        in_stock: 5
-    }, {
-        color: 'Black',
-        size: 2,
-        price: 600,
-        in_stock: 2
-    }, {
-        color: 'Black',
-        size: 3,
-        price: 600,
-        in_stock: 10
-    }, {
-        color: 'Black',
-        size: 4,
-        price: 600,
-        in_stock: 9
-    }, {
-        color: 'Black',
-        size: 5,
-        price: 600,
-        in_stock: 3
-    }]
+  color: 'Blue',
+  brand: 'Basic',
+  size: 1,
+  price: 350,
+  in_stock: 5
+}, {
+  color: 'Blue',
+  brand: 'Converse',
+  size: 2,
+  price: 350,
+  in_stock: 2
+}, {
+  color: 'Blue',
+  brand: 'Fila',
+  size: 3,
+  price: 350,
+  in_stock: 10
+}, {
+  color: 'Blue',
+  brand: 'Jordan',
+  size: 4,
+  price: 350,
+  in_stock: 9
+}, {
+  color: 'Blue',
+  brand: 'Cavella',
+  size: 5,
+  price: 350,
+  in_stock: 3
+}, {
+  color: 'White',
+  brand: 'Lacoste',
+  size: 1,
+  price: 400,
+  in_stock: 5
+}, {
+  color: 'White',
+  brand: 'Omega',
+  size: 2,
+  price: 400,
+  in_stock: 2
+}, {
+  color: 'White',
+  brand: 'DH',
+  size: 3,
+  price: 400,
+  in_stock: 10
+}, {
+  color: 'White',
+  brand: 'CAT',
+  size: 4,
+  price: 400,
+  in_stock: 9
+}, {
+  color: 'White',
+  brand: 'Bronx',
+  size: 5,
+  price: 400,
+  in_stock: 3
+}, {
+  color: 'Black',
+  brand: 'Bata',
+  size: 1,
+  price: 250,
+  in_stock: 5
+}, {
+  color: 'Black',
+  brand: 'Reebok',
+  size: 2,
+  price: 300,
+  in_stock: 20
+}, {
+  color: 'Black',
+  brand: 'Addidas',
+  size: 3,
+  price: 500,
+  in_stock: 10
+}, {
+  color: 'Black',
+  brand: 'Puma',
+  size: 4,
+  price: 600,
+  in_stock: 9
+}, {
+  color: 'Black',
+  brand: 'Nike',
+  size: 5,
+  price: 700,
+  in_stock: 6
+}]
 
 var myTemplate = Handlebars.compile(template.innerHTML);
 searchBtn.addEventListener('click', function() {
-    var sizeSelected = size.value;
-    var colorSelected = colors.value;
-    var shoeList = [];
-    for (var i = 0; i < kids.length; i++) {
-        var selectedList = kids[i];
+  var sizeSelected = size.value;
+  var colorSelected = colors.value;
+  var shoeList = [];
+  for (var i = 0; i < kids.length; i++) {
+    var selectedList = kids[i];
 
-        if (colorSelected === kids[i].color && sizeSelected == kids[i].size) {
-            shoeList.push(selectedList);
-            var shoeInfo = myTemplate({
-                shoeDetails: shoeList
-            });
-            shoeDetails.innerHTML = shoeInfo;
-        } else if (colorSelected === kids[i].color && sizeSelected == "select size") {
-            shoeList.push(selectedList);
-            var shoeInfo = myTemplate({
-                shoeDetails: shoeList
-            });
-            shoeDetails.innerHTML = shoeInfo;
-        } else if (colorSelected === "select color" && sizeSelected == kids[i].size) {
-            shoeList.push(selectedList);
-            var shoeInfo = myTemplate({
-                shoeDetails: shoeList
-            });
-            shoeDetails.innerHTML = shoeInfo;
-        } else if (colorSelected === "select color" && sizeSelected == "select size") {
-            var massage = "You must select size or color!"
-            shoeDetails.innerHTML = massage;
-        }else if (colorSelected === "all" || sizeSelected == "all") {
-            shoeList.push(kids[i]);
-            var shoeInfo = myTemplate({
-                shoeDetails: shoeList
-            });
-            shoeDetails.innerHTML = shoeInfo;
-        }
+    if (colorSelected === kids[i].color && sizeSelected == kids[i].size) {
+      shoeList.push(selectedList);
+      var shoeInfo = myTemplate({
+        shoeDetails: shoeList
+      });
+      shoeDetails.innerHTML = shoeInfo;
+    } else if (colorSelected === kids[i].color && sizeSelected ==
+      "select size") {
+      shoeList.push(selectedList);
+      var shoeInfo = myTemplate({
+        shoeDetails: shoeList
+      });
+      shoeDetails.innerHTML = shoeInfo;
+    } else if (colorSelected === "select color" && sizeSelected == kids[i].size) {
+      shoeList.push(selectedList);
+      var shoeInfo = myTemplate({
+        shoeDetails: shoeList
+      });
+      shoeDetails.innerHTML = shoeInfo;
+    } else if (colorSelected === "select color" && sizeSelected ==
+      "select size") {
+      var massage = "You must select size or color!"
+      shoeDetails.innerHTML = massage;
+    } else if (colorSelected === "all" || sizeSelected == "all") {
+      shoeList.push(kids[i]);
+      var shoeInfo = myTemplate({
+        shoeDetails: shoeList
+      });
+      shoeDetails.innerHTML = shoeInfo;
     }
-    size.value = "select size";
-    colors.value = "select color";
+  }
+  size.value = "select size";
+  colors.value = "select color";
 });
